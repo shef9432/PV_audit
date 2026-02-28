13ed0cefdae3b0e3b94b35769922766dbbba3eb4
shef9432 <137845696+shef9432@users.noreply.github.com>
2026-02-28 19:48:12 +0900

    Refactor image saving and clean up comments

--- core/corruptor.py
+++ core/corruptor.py
@@ -12,18 +12,15 @@ class PhysicalValidator:
 
     def _save(self, img, uid, param_name, val):
         filename = f"{uid}_{param_name}_{val:.3f}.jpg" 
-        save_path = os.path.join(self.output_dir, param_name, filename)
-        cv2.imwrite(save_path, img)
+        cv2.imwrite(os.path.join(self.output_dir, param_name, filename), img)
 
     def apply_illumination(self, img, uid, steps):
-        """Simulates extreme low-light/over-exposure via Gamma Correction."""
         for alpha in steps:
             inv_gamma = 1.0 / alpha if alpha > 0 else 1.0
             table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
             self._save(cv2.LUT(img, table), uid, "Lux", alpha)
 
     def apply_resolution(self, img, uid, steps):
-        """Simulates extreme pixelation (DPI loss)."""
         h, w = img.shape[:2]
         for scale in steps:
             temp = cv2.resize(img, (max(1, int(w*scale)), max(1, int(h*scale))), interpolation=cv2.INTER_AREA)
@@ -31,7 +28,6 @@ class PhysicalValidator:
             self._save(res, uid, "DPI", scale)
 
     def apply_motion_blur(self, img, uid, steps):
-        """Simulates extreme motion blur (Conveyor/Vibration)."""
         for size in steps:
             k = int(size) if int(size) % 2 != 0 else int(size) + 1
             if k < 1: k = 1
@@ -39,13 +35,11 @@ class PhysicalValidator:
             self._save(cv2.filter2D(img, -1, kernel), uid, "MBlur", size)
 
     def apply_defocus(self, img, uid, steps):
-        """Simulates deep optical defocus."""
         for sigma in steps:
             res = img if sigma == 0 else cv2.GaussianBlur(img, (0, 0), sigmaX=sigma)
             self._save(res, uid, "Defocus", sigma)
 
     def apply_sensor_noise(self, img, uid, steps):
-        """Simulates extreme sensor thermal noise (ISO)."""
         for intensity in steps:
             noise = np.random.normal(0, intensity, img.shape).astype('float32')
             res = np.clip(img.astype('float32') + noise, 0, 255).astype('uint8')
